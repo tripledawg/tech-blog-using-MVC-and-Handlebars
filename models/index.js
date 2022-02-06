23e06216975f41c5ae028556a9b31be8866d435a
tripledawg <iamlesliecrouch>
2022-02-05 19:56:06 -0500

    starter files

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Blogpost = require('./Blogpost');
+const Comment = require('./Comment');
+
+
+
+Blogpost.hasMany(Comment, {
+  foreignKey: 'blogpost_id',
+});
+
+Comment.belongsTo(Blogpost);
+
+module.exports = { User, Blogpost, Comment };
