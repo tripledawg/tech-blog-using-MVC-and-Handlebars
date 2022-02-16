61a0d373119635d6fdc7d8d2453946ee51b305a8
tripledawg <iamlesliecrouch>
2022-02-15 19:34:40 -0500

    modified connection.js for jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.JAWS_DB_URL ? process.env.JAWS_DB_URL : process.env.MYSQL_URL
-  //[scheme://][user[:[password]]@]host[:port][/schema]
-);
+  if (process.env.JAWSDB_URL) {
+    
+    const sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    const sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PW,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+      },
+    );
+  } //[scheme://][user[:[password]]@]host[:port][/schema]
 
 module.exports = sequelize;
